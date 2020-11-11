b6d0c9fb00fc1f30dafb78f70b48634d8ac48774
Admin <Admin@DESKTOP-M75UEQH>
2020-11-11 22:02:49 +0700

    11/11/2020 - sửa gì đó.... thêm gì đó, hoàn thành gì đó??????

--- DuAnMain/DuAnMain/Form1.cs
+++ DuAnMain/DuAnMain/Form1.cs
@@ -16,5 +16,10 @@ namespace DuAnMain
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
